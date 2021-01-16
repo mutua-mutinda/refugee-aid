757c792ba8e47a3eb21444066ba1840e2448a079
mutua-mutinda <mutuamutinda@outlook.com>
2021-01-16 12:20:00 +0300

    styled home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,9 +9,18 @@ function Home() {
   return (
     <div className="home">
       <div className="home__slides">
-        <img src={train} alt="" className="mySlides" />
+        {/* <img src={train} alt="" className="mySlides" /> */}
+        <div className="home__content">
+          <p>
+            Refugees Aid aims to provide refugees fleeing their country because
+            of war with shelter, food and health care.
+          </p>
+        </div>
+        <p>Refugee House Settlement</p>
         <img src={hses} alt="" className="mySlides" />
+        <p>Refugee Education</p>
         <img src={edu} alt="" className="mySlides" />
+        <p>Happy Faces</p>
         <img src={educ} alt="" className="mySlides" />
       </div>
     </div>
