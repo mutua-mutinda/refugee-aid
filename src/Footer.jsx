a5ab2cd65b645b092d9e1f89ad193eb36aad25dd
mutua-mutinda <mutuamutinda@outlook.com>
2021-01-30 09:33:29 +0300

    added media query on footer component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import help from "./help.png";
 import "./Footer.css";
-import partner from "./pa-r.png";
+import partner from "./par-r.png";
 
 function Footer() {
   return (
@@ -9,6 +9,9 @@ function Footer() {
       <img src={help} alt="" />
       <div className="footer__name">
         <p>Refugee Aid</p>
+      </div>
+      <div className="footer__slogan">
+        <p>Sharing is Caring</p>
       </div>
       <div className="footer__info">
         <p>In Paternship with</p>
